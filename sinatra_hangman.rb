5cd3fd1807686babd46f01a417dc74a398658677
Tommy <t@tommyhoang.com>
2018-12-07 12:31:26 -0800

    limit response to number of characters in word

--- sinatra_hangman.rb
+++ sinatra_hangman.rb
@@ -10,12 +10,27 @@ end
 
 post '/game' do
   @guess = params[:guess]
-  #validate guess
   session[:guess] = @guess
   redirect "/game?guess=#{@guess}"
 end
 
 get '/game' do
-  @guess1 = session[:guess]
+  @word = session[:word]
+  @guess = session[:guess]
   erb :game
+end
+
+helpers do
+  def set_game
+    @guesses_left = 6
+    @letters_used = []
+    @word = get_random_word
+    # session[:word] = @word
+    @blanks_to_fill = Array.new(@word.length)
+  end
+
+  def get_random_word
+    i = rand(5)
+    ['harry', 'ron', 'snape', 'dumbledore', 'sirius'][i]
+  end
 end
